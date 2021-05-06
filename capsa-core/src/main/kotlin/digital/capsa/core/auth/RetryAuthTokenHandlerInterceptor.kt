3c35eb62085d754bf91cd4ebbed35862199f2721
michaellifTelus <michael.lifshits@telus.com>
2021-05-06 16:45:06 -0400

    multiple scopes implementation

--- capsa-core/src/main/kotlin/digital/capsa/core/auth/RetryAuthTokenHandlerInterceptor.kt
+++ capsa-core/src/main/kotlin/digital/capsa/core/auth/RetryAuthTokenHandlerInterceptor.kt
@@ -15,7 +15,9 @@ class RetryAuthTokenHandlerInterceptor : ClientHttpRequestInterceptor {
     override fun intercept(request: HttpRequest, body: ByteArray, execution: ClientHttpRequestExecution): ClientHttpResponse {
         var response: ClientHttpResponse = execution.execute(request, body)
         if (HttpStatus.UNAUTHORIZED === response.statusCode) {
-            val accessToken: String = clientCredentials.getAuthToken(true)
+            request.headers["Authorization"]
+            // TODO fix hardcoded scope - retrieve from JWT
+            val accessToken: String = clientCredentials.getAuthToken("958", true)
             if (accessToken.isNotBlank()) {
                 request.headers["Authorization"] = accessToken
                 response = execution.execute(request, body)
