cadf2b26ed07fdbbfdc2990c39da40b1c4a8492d
dannymanastireanu <dannymanastireanu@gmail.com>
2021-05-07 14:43:51 +0300

    added getClaim method

--- capsa-core/src/main/kotlin/digital/capsa/core/auth/RetryAuthTokenHandlerInterceptor.kt
+++ capsa-core/src/main/kotlin/digital/capsa/core/auth/RetryAuthTokenHandlerInterceptor.kt
@@ -1,5 +1,7 @@
 package digital.capsa.core.auth
 
+import com.auth0.jwt.JWT
+import com.auth0.jwt.exceptions.JWTDecodeException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpRequest
 import org.springframework.http.HttpStatus
@@ -15,9 +17,8 @@ class RetryAuthTokenHandlerInterceptor : ClientHttpRequestInterceptor {
     override fun intercept(request: HttpRequest, body: ByteArray, execution: ClientHttpRequestExecution): ClientHttpResponse {
         var response: ClientHttpResponse = execution.execute(request, body)
         if (HttpStatus.UNAUTHORIZED === response.statusCode) {
-            val jwtToken = request.headers["Authorization"] // TODO read scope from JWT token
-            val scope = "958"
-            // TODO fix hardcoded scope - retrieve from JWT
+            val jwtToken = request.headers["Authorization"]!!.first() // TODO read scope from JWT token
+            val scope: String = getClaim(jwtToken, "scope")!!
             val accessToken: String = clientCredentials.getAuthToken(scope, true)
             if (accessToken.isNotBlank()) {
                 request.headers["Authorization"] = accessToken
@@ -25,5 +26,17 @@ class RetryAuthTokenHandlerInterceptor : ClientHttpRequestInterceptor {
             }
         }
         return response
+    }
+
+    private fun getClaim(headerAuthorization: String, claimName: String): String? {
+        try {
+            // Get the token, remove the Barrier word
+            val token: String = headerAuthorization.split(" ")[1]
+            val jwt = JWT.decode(token)
+            return jwt.getClaim(claimName).asList(String::class.java).toList().map{it.trim()}.filter { it.isNotEmpty() }.distinct().first()
+        } catch (e: JWTDecodeException) {
+
+        }
+        return null
     }
 }
