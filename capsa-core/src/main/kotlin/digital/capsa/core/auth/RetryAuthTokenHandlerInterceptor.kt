bcf7ce72470e317ef55a58017668f22ea23092fc
dannymanastireanu <dannymanastireanu@gmail.com>
2021-05-07 15:12:28 +0300

    added specific exception to getClaim method.

--- capsa-core/src/main/kotlin/digital/capsa/core/auth/RetryAuthTokenHandlerInterceptor.kt
+++ capsa-core/src/main/kotlin/digital/capsa/core/auth/RetryAuthTokenHandlerInterceptor.kt
@@ -1,7 +1,7 @@
 package digital.capsa.core.auth
 
 import com.auth0.jwt.JWT
-import com.auth0.jwt.exceptions.JWTDecodeException
+import digital.capsa.core.exceptions.AuthTokenException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpRequest
 import org.springframework.http.HttpStatus
@@ -17,8 +17,7 @@ class RetryAuthTokenHandlerInterceptor : ClientHttpRequestInterceptor {
     override fun intercept(request: HttpRequest, body: ByteArray, execution: ClientHttpRequestExecution): ClientHttpResponse {
         var response: ClientHttpResponse = execution.execute(request, body)
         if (HttpStatus.UNAUTHORIZED === response.statusCode) {
-            val jwtToken = request.headers["Authorization"]!!.first() // TODO read scope from JWT token
-            val scope: String = getClaim(jwtToken, "scope")!!
+            val scope: String = getClaim(request.headers["Authorization"], "scope")
             val accessToken: String = clientCredentials.getAuthToken(scope, true)
             if (accessToken.isNotBlank()) {
                 request.headers["Authorization"] = accessToken
@@ -28,15 +27,14 @@ class RetryAuthTokenHandlerInterceptor : ClientHttpRequestInterceptor {
         return response
     }
 
-    private fun getClaim(headerAuthorization: String, claimName: String): String? {
+    private fun getClaim(headerAuthorization: MutableList<String>?, claimName: String): String {
         try {
-            // Get the token, remove the Barrier word
-            val token: String = headerAuthorization.split(" ")[1]
+            // Get the token, remove the Bearer word
+            val token: String = headerAuthorization!!.first().split(" ")[1]
             val jwt = JWT.decode(token)
             return jwt.getClaim(claimName).asList(String::class.java).toList().map{it.trim()}.filter { it.isNotEmpty() }.distinct().first()
-        } catch (e: JWTDecodeException) {
-
+        } catch (e: Exception){
+            throw AuthTokenException("Fail to extract $claimName from jwt token!", e)
         }
-        return null
     }
 }
