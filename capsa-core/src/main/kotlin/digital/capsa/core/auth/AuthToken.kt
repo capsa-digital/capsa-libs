5e19db6f8156b61d13ce520632362759ef8ec183
dannymanastireanu <dannymanastireanu@gmail.com>
2021-05-10 20:08:44 +0300

    UTC -> ZoneDateTime

--- capsa-core/src/main/kotlin/digital/capsa/core/auth/AuthToken.kt
+++ capsa-core/src/main/kotlin/digital/capsa/core/auth/AuthToken.kt
@@ -1,11 +1,12 @@
 package digital.capsa.core.auth
 
 import java.time.Instant
+import java.time.ZonedDateTime
 import java.util.concurrent.atomic.AtomicInteger
 
 data class AuthToken(
     val token: String,
     val tokenType: String,
-    val expiryDate: Instant,
+    val expiryDate: ZonedDateTime,
     val usageCounter: AtomicInteger = AtomicInteger(0)
 )
