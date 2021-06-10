5f06f9620ad105382af09aa0b371d4bf65b1d977
Michael Lifshits <michael.lifshits@telus.com>
2021-06-10 11:51:17 -0400

    Define RetryAuthTokenHandlerInterceptor and ClientCredentials as @Configuration

--- capsa-core/src/main/kotlin/digital/capsa/core/auth/RetryAuthTokenHandlerInterceptor.kt
+++ capsa-core/src/main/kotlin/digital/capsa/core/auth/RetryAuthTokenHandlerInterceptor.kt
@@ -5,18 +5,24 @@ import com.auth0.jwt.exceptions.JWTDecodeException
 import digital.capsa.core.exceptions.AuthTokenException
 import digital.capsa.core.logger
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpRequest
 import org.springframework.http.HttpStatus
 import org.springframework.http.client.ClientHttpRequestExecution
 import org.springframework.http.client.ClientHttpRequestInterceptor
 import org.springframework.http.client.ClientHttpResponse
 
+@Configuration
 class RetryAuthTokenHandlerInterceptor : ClientHttpRequestInterceptor {
 
     @Autowired
     private lateinit var clientCredentials: ClientCredentials
 
-    override fun intercept(request: HttpRequest, body: ByteArray, execution: ClientHttpRequestExecution): ClientHttpResponse {
+    override fun intercept(
+        request: HttpRequest,
+        body: ByteArray,
+        execution: ClientHttpRequestExecution
+    ): ClientHttpResponse {
         var response: ClientHttpResponse = execution.execute(request, body)
         if (HttpStatus.UNAUTHORIZED === response.statusCode) {
             val scope: String = getClaim(request.headers["Authorization"], "scope")
@@ -35,7 +41,8 @@ class RetryAuthTokenHandlerInterceptor : ClientHttpRequestInterceptor {
             // Get the token, remove the Bearer word
             val token: String = headerAuthorization?.first()?.split(" ")?.get(1)
                 ?: throw AuthTokenException("Failed to extract Authorization token!", null)
-            return JWT.decode(token).getClaim(claimName).asList(String::class.java).map { it.trim() }.filter { it.isNotEmpty() }.distinct().first()
+            return JWT.decode(token).getClaim(claimName).asList(String::class.java).map { it.trim() }
+                .filter { it.isNotEmpty() }.distinct().first()
         } catch (e: JWTDecodeException) {
             throw AuthTokenException("Failed to extract $claimName from jwt token!", e)
         }
