8f59c71cc9f90e22f2eaa9d5229bc9bd73c0a803
Michael Lifshits <michael.lifshits@telus.com>
2021-06-15 06:29:10 -0400

    Update Spring Boot version SPRING_BOOT = "2.4.7" and SPRING_CLOUD = "2020.0.1"

--- buildSrc/src/main/kotlin/CoreVersions.kt
+++ buildSrc/src/main/kotlin/CoreVersions.kt
@@ -7,10 +7,10 @@ object CoreVersion {
     const val JAXB_API = "2.3.1"
     const val JAXB_IMPL = "3.0.0"
     const val LOGBACK_CONTRIB = "0.1.5"
-    const val OPENAPI = "1.5.3"
+    const val OPENAPI = "1.5.8"
     const val REACTOR_BOM = "Californium-RELEASE"
     const val SELENIUM = "3.141.59"
-    const val SPRING_BOOT = "2.4.1"
-    const val SPRING_CLOUD = "Hoxton.SR5"
+    const val SPRING_BOOT = "2.4.7"
+    const val SPRING_CLOUD = "2020.0.1"
     const val WIREMOCK_JRE8 = "2.27.2"
 }
