a357eba40ee325d7466f5c1704450933536a018c
Michael Lifshits <michael.lifshits@telus.com>
2021-07-28 10:16:33 -0400

    removing internal

--- capsa-core/src/main/kotlin/digital/capsa/core/fileparser/FileParserDsl.kt
+++ capsa-core/src/main/kotlin/digital/capsa/core/fileparser/FileParserDsl.kt
@@ -6,7 +6,7 @@ import java.io.BufferedReader
 annotation class ParserMarker
 
 @ParserMarker
-internal class Parser(private val bufferedReader: BufferedReader) {
+class Parser(private val bufferedReader: BufferedReader) {
 
     private val lines: List<Line> = bufferedReader.lineSequence().map {
         Line(it, null)
@@ -15,13 +15,13 @@ internal class Parser(private val bufferedReader: BufferedReader) {
     }
 
     fun header(
-        recordBuilder: TransformerBuilder.() -> Any
+            recordBuilder: TransformerBuilder.() -> Any
     ) {
         lines[0].obj = TransformerBuilder(lines[0].str, 0).recordBuilder()
     }
 
     fun line(
-        recordBuilder: TransformerBuilder.() -> Any
+            recordBuilder: TransformerBuilder.() -> Any
     ) {
         lines.forEachIndexed { index, line ->
             if (index != 0) {
@@ -35,21 +35,21 @@ internal class Parser(private val bufferedReader: BufferedReader) {
     }
 
     private data class Line(
-        val str: String,
-        var obj: Any?
+            val str: String,
+            var obj: Any?
     )
 }
 
-internal class TransformerBuilder(
-    val line: String,
-    val index: Int
+class TransformerBuilder(
+        val line: String,
+        val index: Int
 ) {
     inline fun <reified R> field(
-        name: String,
-        from: Int,
-        toExclusive: Int,
-        default: () -> R? = { null },
-        noinline parser: ((String) -> R?)? = null
+            name: String,
+            from: Int,
+            toExclusive: Int,
+            default: () -> R? = { null },
+            noinline parser: ((String) -> R?)? = null
     ): R? {
         val str = readField(from, toExclusive)
         return if (str.isNotBlank()) {
@@ -58,11 +58,11 @@ internal class TransformerBuilder(
     }
 
     inline fun <reified R> mandatoryField(
-        name: String,
-        from: Int,
-        toExclusive: Int,
-        default: () -> R = { throw FileParserException("The field $name is mandatory") },
-        noinline parser: ((String) -> R)? = null
+            name: String,
+            from: Int,
+            toExclusive: Int,
+            default: () -> R = { throw FileParserException("The field $name is mandatory") },
+            noinline parser: ((String) -> R)? = null
     ): R {
         val str = readField(from, toExclusive)
         return if (str.isNotBlank()) {
@@ -70,15 +70,15 @@ internal class TransformerBuilder(
         } else default.invoke()
     }
 
-    private fun readField(from: Int,
+    fun readField(from: Int,
                           toExclusive: Int): String {
         return if (line.length >= toExclusive) line.substring(from, toExclusive).trim() else ""
     }
 }
 
-internal fun parser(
-    bufferedReader: BufferedReader,
-    init: Parser.() -> Unit
+fun parser(
+        bufferedReader: BufferedReader,
+        init: Parser.() -> Unit
 ): Parser {
     val parser = Parser(bufferedReader)
     parser.init()
@@ -86,4 +86,4 @@ internal fun parser(
 }
 
 open class FileParserException(message: String, cause: Throwable? = null) :
-    RuntimeException(message, cause)
+        RuntimeException(message, cause)
