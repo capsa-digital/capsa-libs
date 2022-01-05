9a36068c0cf5f87ba88fdeaa1a12879ef7c989c4
Marian Puscasu <marian.puscasu@telus.com>
2022-01-05 17:21:50 +0200

    Update dependency and Gradle versions

--- buildSrc/src/main/kotlin/CoreVersions.kt
+++ buildSrc/src/main/kotlin/CoreVersions.kt
@@ -1,15 +1,15 @@
 @file:Suppress("detekt.StringLiteralDuplication")
 
 object CoreVersion {
-    const val ASSERTK_JVM = "0.23.1"
-    const val GUAVA = "30.1-jre"
+    const val ASSERTK_JVM = "0.25"
+    const val GUAVA = "31.0.1-jre"
     const val JAVA_HAMCREST = "2.0.0.0"
     const val JAXB_API = "2.3.1"
-    const val JAXB_IMPL = "3.0.0"
-    const val JMETER = "5.4.1"
-    const val JMETER_JPGC_CASUTG = "2.9"
+    const val JAXB_IMPL = "3.0.2"
+    const val JMETER = "5.4.3"
+    const val JMETER_JPGC_CASUTG = "2.10"
     const val LOGBACK_CONTRIB = "0.1.5"
-    const val OPENAPI = "1.5.8"
+    const val OPENAPI = "1.6.3"
     const val REACTOR_BOM = "Californium-RELEASE"
-    const val WIREMOCK_JRE8 = "2.27.2"
+    const val WIREMOCK_JRE8 = "2.32.0"
 }
