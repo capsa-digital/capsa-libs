b0eddd77021d7f7157be32567750ceeb9c5b8c55
Michael Lifshits <michael.lifshits@telus.com>
2022-02-14 09:18:23 -0500

    Restoring PerformanceService

---
+++ capsa-perfrunner/src/main/kotlin/digital/capsa/perfrunner/PerformanceService.kt
@@ -0,0 +1,23 @@
+package digital.capsa.perfrunner
+
+import digital.capsa.perfrunner.domain.Plan
+import digital.capsa.perfrunner.domain.Report
+import org.springframework.context.annotation.Bean
+import org.springframework.stereotype.Service
+
+@Service
+class PerformanceService {
+
+    @Bean
+    fun executePlan(): (Plan) -> Report = { plan ->
+        val jMeterSummariser: JMeterSummariser = JMeterRunner.execute(plan)
+        Report(
+            jMeterSummariser.getSummary(),
+            jMeterSummariser.getNumSamples(),
+            jMeterSummariser.getErrorCount(),
+            jMeterSummariser.getThroughput(),
+            jMeterSummariser.getAverage(),
+            jMeterSummariser.getMaxResponseTime()
+        )
+    }
+}
